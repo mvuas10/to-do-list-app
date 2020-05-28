485b0276af95d31722507a4eee287168a495fc4a
mvuas10 <mvuas10@gmail.com>
2020-05-28 13:52:24 +0200

    Add new information to DB without using seeds

--- models/todoitem.js
+++ models/todoitem.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const todoitem = sequelize.define('todoitem', {
-    task: DataTypes.STRING,
-    deadline: DataTypes.STRING,
-    important: DataTypes.BOOLEAN;
-  }, {});
-  todoitem.associate = function(models) {
+  const todoitem = sequelize.define(
+    "todoitem",
+    {
+      task: DataTypes.STRING,
+      deadline: DataTypes.STRING,
+      important: DataTypes.BOOLEAN,
+    },
+    {}
+  );
+  todoitem.associate = function (models) {
     // associations can be defined here
   };
   return todoitem;
